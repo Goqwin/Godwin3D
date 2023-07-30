d10abe57ac04d6af91de148432edc8560f27ad22
Godwin <115321545+Goqwin@users.noreply.github.com>
2023-07-30 00:23:29 -0600

    Updated Contact.jsx
    Added my Resume which is another repo and changed the connections to circles. Rel-labelled the relationship and targets of the links.

--- Godwin3D/src/components/Contact.jsx
+++ Godwin3D/src/components/Contact.jsx
@@ -1,12 +1,10 @@
-import {useState, useRef } from 'react';
-import {motion} from 'framer-motion';
+import React, { useState, useRef } from 'react';
+import { motion } from 'framer-motion';
 import emailjs from '@emailjs/browser';
 
 import { styles } from "../styles";
 import { SectionWrapper } from '../hoc';
 import { slideIn } from '../utils/motion';
-
-
 
 // Object Functionality
 const Contact = () => {
@@ -116,19 +114,25 @@ const Contact = () => {
         </form>
 
         <p className={styles.sectionSubText}>Socials: Connect with Me</p>
-        <div className='xl:mt-12 flex xl:flex-row flex-col-reverse gap-10 overflow-hidden'>
-          <a href="https://discord.com/users/goqwin" target="_blank" rel="noopener noreferrer">
-            <img src={"https://logodownload.org/wp-content/uploads/2017/11/discord-logo-0.png"} alt="Discord" className="bg-white rounded-full w-20 h-15 border-2 border-black hover:scale-110" />
-          </a>
-          <a href="https://www.linkedin.com/in/godwin-jb-mercado-346727237/" target="_blank" rel="noopener noreferrer">
-            <img src={"https://th.bing.com/th/id/R.21fba12531ac377e1642a01921db9fbd?rik=ErbH90Kfy3L6tg&riu=http%3a%2f%2fencomium.ng%2fwp-content%2fuploads%2f2016%2f01%2fLinkedIn-Logo.png&ehk=qo9l10kLERwTTr5WEWyHeVR%2bKIWh4CchVBnnAxyz3ic%3d&risl=&pid=ImgRaw&r=0"} alt="LinkedIn" className="bg-white rectangle-md w-20 h-25 border-2 border-black hover:scale-110" />
-          </a>
-          <a href="https://github.com/Goqwin" target="_blank" rel="noopener noreferrer">
-            <img src={"https://logos-world.net/wp-content/uploads/2020/11/GitHub-Logo.png"} alt="GitHub" className="bg-white rounded-lg w-20 h-20 border-2 border-black hover:scale-110" />
-          </a>
-        </div>
-      </motion.div> 
+        <div class="xl:mt-12 flex xl:flex-row flex-col-reverse gap-10 overflow-hidden">
+  <a href="https://discord.com/users/goqwin" target="_blank" rel="noopener noreferrer">
+    <img src="https://logodownload.org/wp-content/uploads/2017/11/discord-logo-0.png" alt="Discord" class="bg-white rounded-full w-20 h-20 border-2 border-black hover:scale-110" />
+  </a>
+  <a href="https://www.linkedin.com/in/godwin-jb-mercado-346727237/" target="_blank" rel="noopener noreferrer">
+    <img src="https://th.bing.com/th/id/R.21fba12531ac377e1642a01921db9fbd?rik=ErbH90Kfy3L6tg&riu=http%3a%2f%2fencomium.ng%2fwp-content%2fuploads%2f2016%2f01%2fLinkedIn-Logo.png&ehk=qo9l10kLERwTTr5WEWyHeVR%2bKIWh4CchVBnnAxyz3ic%3d&risl=&pid=ImgRaw&r=0" alt="LinkedIn" class="bg-white rounded-full w-20 h-20 border-2 border-black hover:scale-110" />
+  </a>
+  <a href="https://github.com/Goqwin" target="_blank" rel="noopener noreferrer">
+    <img src="https://logos-world.net/wp-content/uploads/2020/11/GitHub-Logo.png" alt="GitHub" class="bg-white rounded-full w-20 h-20 border-2 border-black hover:scale-110" />
+  </a>
+  <a href="https://goqwin.github.io/Resume/" target="_blank" rel="noopener noreferrer">
+    <div class="bg-white rounded-full w-20 h-20 border-2 border-black hover:scale-110">
+      <p class="text-black text-center font-medium">MY RESUME</p>
+    </div>
+  </a>
+</div>
+      </motion.div>
     </div>
   );
 };
+
 export default SectionWrapper(Contact, "contact");
